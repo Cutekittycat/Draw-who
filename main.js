e5041d7d6726a485921c1dc32c76b2e52bb12f5e
Cutekittycat <81061612+Cutekittycat@users.noreply.github.com>
2021-10-09 16:05:23 +1300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function setup(){
+    canvas = createCanvas(500, 500);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.SpeechSynthesis;
+}
+
+function draw(){
+strokeWeight(8);
+stroke(0);
+if (mouseIsPressed) {
+    line(pmouseX, pmouseY , mouseX, mouseY);
+}
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function clearCanvas(){
+
+    background("white");
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
